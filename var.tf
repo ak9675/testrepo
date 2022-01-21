fd2fb8b9d216b16e5489007cfa1bcb2dbe0028ad
anshu <myemailforyouin@gmail.com>
2022-01-21 06:04:38 +0000

    first commit

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "rg_name" {
+    default = "rg1"
+}
+
+variable "rg_location" {
+    default = "eastus"
+}
+
+variable "size" {
+    default = "Standard_F2"
+}
+
+variable "username" {
+    default = "admin"
+}
